f7a06f75f5aaf4c735f37ff30ec95300016d0c39
Admin <Admin@DESKTOP-J4NQSF1>
2024-10-22 15:20:53 +0500

    Add project files.

---
+++ Project2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project2
+{
+    class Programm  
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+            string action;
+            Console.WriteLine("Amal 1");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Amal 2");
+            secondValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("'+''-''/''*''%'");
+            action = Console.ReadLine();
+
+            switch(action)
+            {
+                case "+":
+                Console.WriteLine(firstValue+secondValue);
+                    break;
+
+                case "-":
+                    Console.WriteLine(firstValue-secondValue);
+                    break;
+                case "*":
+                    Console.WriteLine(firstValue*secondValue);
+                    break;
+                    case "/":
+                    Console.WriteLine(firstValue/secondValue);
+                    break;
+                    case "%":
+                
+                    Console.WriteLine(firstValue+secondValue);
+
+                    if(secondValue == 0)
+                        Console.WriteLine(0);
+
+                    else
+                        Console.WriteLine(firstValue%secondValue);
+                    break;
+                default:
+                    Console.WriteLine("ERoR");
+                    break;
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
